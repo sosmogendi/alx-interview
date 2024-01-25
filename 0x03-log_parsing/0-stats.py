66d15adeee57455b02b803aa0e1b7b1c9356e43e
Sospeter <sosmogendi@gmail.com>
2024-01-25 07:52:44 -0800

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,36 @@
 #!/usr/bin/python3
+'''A script that reads stdin line by line and computes metrics'''
+
 import sys
 
-def print_statistics(total_file_size, status_code_counts):
-    print("Total file size: {}".format(total_file_size))
-    for code in sorted(status_code_counts):
-        count = status_code_counts[code]
-        print("{}: {}".format(code, count))
+status_code_counts = {'200': 0, '301': 0, '400': 0, '401': 0,
+                      '403': 0, '404': 0, '405': 0, '500': 0}
+total_file_size = 0
+line_counter = 0
 
-def parse_log_line(line):
-    try:
-        parts = line.split()
-        if len(parts) == 9:
-            status_code = int(parts[8])
-            file_size = int(parts[7])
+try:
+    for line in sys.stdin:
+        line_parts = line.split(" ")
+        if len(line_parts) > 4:
+            code = line_parts[-2]
+            size = int(line_parts[-1])
+            if code in status_code_counts:
+                status_code_counts[code] += 1
+            total_file_size += size
+            line_counter += 1
 
-            if status_code in status_code_counts:
-                return status_code, file_size
-    except (ValueError, IndexError):
-        pass
-    return None, None
+        if line_counter == 10:
+            line_counter = 0
+            print('File size: {}'.format(total_file_size))
+            for key, value in sorted(status_code_counts.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-def main():
-    total_file_size = 0
-    status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    line_count = 0
+except KeyboardInterrupt:
+    pass
 
-    try:
-        for line in sys.stdin:
-            status_code, file_size = parse_log_line(line)
-            if status_code is not None and file_size is not None:
-                total_file_size += file_size
-                status_code_counts[status_code] += 1
-
-                line_count += 1
-                if line_count == 10:
-                    print_statistics(total_file_size, status_code_counts)
-                    line_count = 0
-    except KeyboardInterrupt:
-        pass
-    except BrokenPipeError:
-        # Ignore Broken Pipe Error when the output is closed
-        pass
-
-    # Print final statistics before exiting
-    print_statistics(total_file_size, status_code_counts)
-
-if __name__ == "__main__":
-    main()
+finally:
+    print('File size: {}'.format(total_file_size))
+    for key, value in sorted(status_code_counts.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
