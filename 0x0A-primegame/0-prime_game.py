c389a9a331428c69336d311d1d02acbb5281e6ce
Sospeter <sosmogendi@gmail.com>
2024-03-14 13:02:54 -0700

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,56 +1,49 @@
 #!/usr/bin/python3
 
-"""Prime Game Algorithm Python"""
-
-
-def is_prime(n):
-    """Check if a number n is prime"""
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def calculate_primes(n, primes):
-    """Calculate all primes up to n and store in the primes list"""
-    top_prime = primes[-1]
-    if n > top_prime:
-        for i in range(top_prime + 1, n + 1):
-            if is_prime(i):
-                primes.append(i)
-            else:
-                primes.append(0)
+def generate_primes(n):
+    """Generate a list of prime numbers up to n using Sieve of Eratosthenes algorithm."""
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False
+    p = 2
+    while p * p <= n:
+        if primes[p]:
+            for i in range(p * p, n + 1, p):
+                primes[i] = False
+        p += 1
+    return [i for i in range(n + 1) if primes[i]]
 
 
 def isWinner(x, nums):
     """
     Determine the winner of each round based on the rules provided.
-    x is the number of rounds and nums is an array of n
-    Return: name of the player that won the most rounds
-    If the winner cannot be determined, return None
-    You can assume n and x will not be larger than 10000
+    :param x: Number of rounds
+    :param nums: An array of n for each round
+    :return: Name of the player that won the most rounds, or None if winner cannot be determined
     """
-    players_wins = {"Maria": 0, "Ben": 0}
-    primes = [0, 0, 2]  # Initialize with first three primes
+    def can_player_win(n, primes):
+        if n < 2:
+            return False
+        if n in primes:
+            return True
+        if n % 2 == 0:
+            return False
+        return True
 
-    calculate_primes(max(nums), primes)
+    max_num = max(nums)
+    primes = generate_primes(max_num)
+    maria_wins = 0
+    ben_wins = 0
 
-    for num in nums:
-        sum_options = sum(1 for p in primes[:num + 1] if p != 0)
+    for n in nums:
+        if can_player_win(n, primes):
+            if n % 2 == 0:
+                ben_wins += 1
+            else:
+                maria_wins += 1
 
-        if sum_options % 2:
-            winner = "Maria"
-        else:
-            winner = "Ben"
-
-        if winner:
-            players_wins[winner] += 1
-
-    if players_wins["Maria"] > players_wins["Ben"]:
+    if maria_wins > ben_wins:
         return "Maria"
-    elif players_wins["Ben"] > players_wins["Maria"]:
+    elif ben_wins > maria_wins:
         return "Ben"
     else:
         return None
