310d60155d38adcc82523426ea58a6a9f8b1a7e6
Sospeter <sosmogendi@gmail.com>
2024-03-24 09:53:38 -0700

    Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,17 +12,16 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    triangle = []
 
-    for i in range(1, n):
-        row = [1]
+    for i in range(n):
+        row = [None] * (i + 1)
+        row[0] = 1
+        row[-1] = 1
 
-        for j in range(1, i):
-            # Calculate each element based on the sum of the two elements above it
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        for j in range(1, len(row) - 1):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
 
-        row.append(1)
         triangle.append(row)
 
     return triangle
-
